6caad9a2c23ace7533e5f9e8143802fa7b5eb4ca
Marcelo Schild <marceloschild90@gmail.com>
2019-04-26 02:13:33 -0300

    - Added tictactoe and board components - Added basic navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TicTacToeComponent } from './tic-tac-toe/components/tic-tac-toe/tic-tac-toe.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+  path: '',
+  children: [
+    { path: '', component: TicTacToeComponent },
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/tic-tac-toe/tic-tac-toe.module.ts
+++ src/app/tic-tac-toe/tic-tac-toe.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from './services/user.service';
 import { GameService } from './services/game.service';
+import { TicTacToeComponent } from './components/tic-tac-toe/tic-tac-toe.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TicTacToeComponent, BoardComponent],
   imports: [
     CommonModule
   ],
